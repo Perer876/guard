6ce89669b4df7fff51a65b4069c7b69fcc91469f
Oscar Arambula <peteroso876@gmail.com>
2025-07-07 23:28:50 -0600

    feat(JustInTimeGuard): correctly determines whether a permission is granted to a subject
    it uses reflection and do caches nothing

--- src/JustInTimeGuard.php
+++ src/JustInTimeGuard.php
@@ -5,16 +5,42 @@ declare(strict_types=1);
 namespace Guard;
 
 use Override;
+use ReflectionAttribute;
+use ReflectionEnum;
+use ReflectionException;
+use SplObjectStorage;
 
 /**
+ * Just-in-time guard that checks if a subject has the permission based on the roles granted to that permission.
+ * This guard uses reflection to dynamically check the roles associated with a permission at runtime.
+ *
  * @internal
  */
 final readonly class JustInTimeGuard implements Guard
 {
+    /**
+     * @throws ReflectionException
+     */
     #[Override]
     public function can(Subject $subject, Permission $permission): bool
     {
-        // TODO: Implement the logic to check if the subject has the specified permission.
-        return false;
+        $enumReflection = new ReflectionEnum($permission::class);
+        $caseReflection = $enumReflection->getCase($permission->name);
+
+        $grantedRoles = new SplObjectStorage();
+
+        /** @var ReflectionAttribute<GrantToAttribute>[] $grantsToAttribute $grantsTo */
+        $grantsToAttribute = [
+            ...$enumReflection->getAttributes(GrantToAttribute::class, ReflectionAttribute::IS_INSTANCEOF),
+            ...$caseReflection->getAttributes(GrantToAttribute::class, ReflectionAttribute::IS_INSTANCEOF),
+        ];
+
+        foreach ($grantsToAttribute as $grantToAttribute) {
+            /** @var GrantToAttribute $grantTo */
+            $grantTo = $grantToAttribute->newInstance();
+            $grantedRoles->addAll($grantTo->roles);
+        }
+
+        return array_any($subject->getRoles(), static fn(Role $role): bool => $grantedRoles->contains($role));
     }
 }
